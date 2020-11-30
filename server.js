05e49973a8977efd99b4e0f492e265ae5b2c77f3
Keith Fung <fungsenkeith@gmail.com>
2020-11-30 03:08:44 -0500

    added mongo usability

--- server.js
+++ server.js
@@ -3,26 +3,33 @@ const logger = require("morgan");
 const mongoose = require("mongoose");
 const compression = require("compression");
 
-const PORT = 3000;
-
+// Establish PORT Connection
+const PORT = process.env.PORT || 3000;
 const app = express();
 
+// Middleware
 app.use(logger("dev"));
 
 app.use(compression());
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
+// Set static files to public folder
 app.use(express.static("public"));
 
-mongoose.connect("mongodb://localhost/budget", {
+// Mongoose connect (workout database)
+// https://mongoosejs.com/docs/deprecations.html
+// process.env.MONGODB_URI || <- removed from mongoose.connect()
+mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/budget", {
   useNewUrlParser: true,
-  useFindAndModify: false
+  useFindAndModify: false,
+  useUnifiedTopology: true,
 });
 
-// routes
+// Routes
 app.use(require("./routes/api.js"));
 
+// Initiate server
 app.listen(PORT, () => {
   console.log(`App running on port ${PORT}!`);
 });
